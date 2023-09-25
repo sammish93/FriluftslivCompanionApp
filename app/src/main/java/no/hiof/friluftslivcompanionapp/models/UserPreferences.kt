422e6e2c8939fc40c646331c16893223705ced48
Sammish <samd93@hotmail.co.uk>
2023-09-25 15:58:42 +0200

    added new display picture parameter to UserPreferences class

--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/UserPreferences.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/UserPreferences.kt
@@ -1,8 +1,10 @@
 package no.hiof.friluftslivcompanionapp.models
 
+import no.hiof.friluftslivcompanionapp.models.enums.DisplayPicture
 import no.hiof.friluftslivcompanionapp.models.enums.SupportedLanguage
 
 data class UserPreferences(
+    var displayPicture: DisplayPicture = DisplayPicture.DP_DEFAULT,
     var darkModeEnabled: Boolean = false,
     var language: SupportedLanguage = SupportedLanguage.ENGLISH
 )
--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/enums/DisplayPicture.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/enums/DisplayPicture.kt
@@ -3,6 +3,12 @@ package no.hiof.friluftslivcompanionapp.models.enums
 import no.hiof.friluftslivcompanionapp.R
 
 enum class DisplayPicture(val resourcePaths: Map<String, Int>) {
+    DP_DEFAULT(
+        mapOf(
+            "defaultResolution" to R.drawable.ic_launcher_foreground,
+            "alternateResolution" to R.drawable.ic_launcher_background
+        )
+    ),
     DP_ONE(
         mapOf(
             "defaultResolution" to R.drawable.ic_launcher_foreground,
