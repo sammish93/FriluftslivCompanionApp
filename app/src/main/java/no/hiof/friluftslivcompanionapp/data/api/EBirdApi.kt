fe7f52eb87c0ceb8a4892d964c4d1a3e798be7bc
stian96 <stiankru@hiof.no>
2023-09-27 16:20:33 +0200

    wrote doc comments for the EBirdApi

--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/api/EBirdApi.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/api/EBirdApi.kt
@@ -10,9 +10,20 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Exception
 
+/**
+ * The `EBirdApi` class is responsible for interacting with the eBird API.
+ * It uses Retrofit to make network requests and Gson to parse the JSON responses.
+ *
+ * @property baseUrl The base URL for the eBird API.
+ * @property retrofit The Retrofit instance used to create the eBird API Service instance.
+ * @property eBirdApiService The service instance used to make API calls.
+ *
+ * This class provides a method to get recent bird observations and map them to `Bird` objects.
+ * The method `getRecentObservations` is a suspending function and should be called from a coroutine
+ * or another suspending function.
+ */
 class EBirdApi {
 
-    // Base url for the eBird API.
     private val baseUrl = "https://api.ebird.org/"
 
     // Retrofit instance.
