403f99a5799c50f67084551d8bac4d948bc11d4a
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-09-28 17:20:26 +0200

    Added Card Component to TipsScreen instead of ProfileScreen

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
@@ -1,6 +1,14 @@
 package no.hiof.friluftslivcompanionapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,14 +16,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import no.hiof.friluftslivcompanionapp.data.CardItem.cardItems
+import no.hiof.friluftslivcompanionapp.ui.components.CardComponent
 import no.hiof.friluftslivcompanionapp.ui.theme.CustomTypography
 
 @Composable
 fun TripsScreen(modifier: Modifier = Modifier) {
-    Text(text = "This is the Trips screen!",
-        style = CustomTypography.headlineLarge,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center,
-        modifier = modifier.fillMaxSize()
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "This is the Trip screen!",
+            style = CustomTypography.headlineLarge,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            items(cardItems) { cardItem ->
+                CardComponent(cardItem)
+            }
+        }
+    }
 }
