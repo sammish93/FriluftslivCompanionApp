8a66c7b0c73e5075c17744057ec5e6768629ed02
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-09-28 20:09:58 +0200

    changed list components to be more reusable

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/ListComponent.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/ListComponent.kt
@@ -3,15 +3,16 @@ package no.hiof.friluftslivcompanionapp.ui.components
 import ListItemComponent
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 import no.hiof.friluftslivcompanionapp.ui.components.items.CardItem
 
-@Composable
-fun ListComponent (items: List<CardItem>){
-    LazyColumn{
-        items(items){item->
-            ListItemComponent(item)
-        }
-    }
 
+@Composable
+fun <T> ListComponent(items: List<T>,
+                      itemContent:@Composable (T, TextStyle)->Unit) {
+        items.forEach{item->
+            itemContent(item, MaterialTheme.typography.headlineLarge)
+        }
 }
--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/items/ListItem.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/items/ListItem.kt
@@ -3,12 +3,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import no.hiof.friluftslivcompanionapp.ui.components.items.CardItem
 
 @Composable
-fun ListItemComponent(item: CardItem) {
-    Text (text = item.title, modifier = Modifier.padding(16.dp))
+fun <T>ListItemComponent(item: T, textStyle: TextStyle, displayText:(T) ->String) {
+    Text (text = displayText(item), style= textStyle,modifier = Modifier.padding(16.dp))
 }
 
 
