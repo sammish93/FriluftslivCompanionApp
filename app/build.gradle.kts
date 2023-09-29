ff36ad538c2e5dcfa69269f290173e2109e8d5e8
Sammish <samd93@hotmail.co.uk>
2023-09-29 11:22:13 +0200

    commented out previous material design dependency that was outdated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     // Possibly unnecessary - was used for BottomNavigation but they renamed it.
-    implementation("androidx.compose.material:material:1.5.1")
+    //implementation("androidx.compose.material:material:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
