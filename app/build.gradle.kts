6e6e389a04eca343c3c52a85bea8786f1ce7910f
Sammish <samd93@hotmail.co.uk>
2023-09-29 09:27:40 +0200

    added comment to dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    // Possibly unnecessary - was used for BottomNavigation but they renamed it.
     implementation("androidx.compose.material:material:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
