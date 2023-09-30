cb4f128645972b18dee8c36b88fe92a652bc268b
stian96 <stiankru@hiof.no>
2023-09-30 18:45:09 +0200

    working on unit tests.

--- app/src/test/java/no/hiof/friluftslivcompanionapp/BirdObservationsTest.kt
+++ app/src/test/java/no/hiof/friluftslivcompanionapp/BirdObservationsTest.kt
@@ -46,4 +46,28 @@ class BirdObservationsTest {
         assertEquals(expected.value[0].observationDate, observations[0].observationDate)
         assertEquals(expected.value[0].coordinates, observations[0].coordinates)
     }
+
+    // TODO(Fix this test later)
+    @Test
+    fun getRecentObservations_returnsResultInNorwegian() = runBlocking {
+
+        val expected = Result.Success(listOf(
+            Bird(
+                speciesName = "lomvi",
+                speciesNameScientific = "Uria aalge",
+                number = 16,
+                photoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Common_Murre_Uria_aalge.jpg/600px-Common_Murre_Uria_aalge.jpg",
+                description = """
+                    Lomvi eller ringvi (Uria aalge) er en pelagisk dykkende sjøfugl og den største av alkene (Alcini), en monofyletisk gruppe som tilhører familien alkefugler (Alcidae). Arten finnes i tempererte og lavarktiske kyststrøk i nordområdene i Atlanterhavet og Stillehavet.
+                    Norsk lomvi hekker først og fremst på Bjørnøya, der det finnes omkring 100 000 par av denne arten. Noen få kolonier hekker også på Spitsbergen, henholdsvis ved Prins Karls Forland og Amsterdamøya.
+                """.trimIndent(),
+                observationDate = LocalDateTime.of(2023, 9, 30, 16, 37),
+                coordinates = Location(lat = 59.904504, lon = 10.753352)
+            )
+        ))
+
+
+        val result = birdObservations.getRecentObservations(year=2023, month=9, day=30)
+        assertEquals(expected, result)
+    }
 }
