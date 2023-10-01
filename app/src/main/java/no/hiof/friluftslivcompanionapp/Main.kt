80e26eaa39e6d45ad3d24ac7af46d68babb8d4f1
stian96 <stiankru@hiof.no>
2023-10-01 16:06:05 +0200

    .

--- app/src/main/java/no/hiof/friluftslivcompanionapp/Main.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/Main.kt
@@ -2,16 +2,17 @@ package no.hiof.friluftslivcompanionapp
 
 import no.hiof.friluftslivcompanionapp.data.network.Result
 import no.hiof.friluftslivcompanionapp.domain.BirdObservations
-import no.hiof.friluftslivcompanionapp.models.enums.SupportedLanguage
 import kotlin.system.exitProcess
 
 suspend fun main() {
 
-    val api = BirdObservations.getInstance(SupportedLanguage.ENGLISH)
-    val result = api.getRecentObservations(maxResult = 10)
+    val api = BirdObservations.getInstance()
+    val result = api.getRecentObservations(year = 2024, month = 5, day = 17)
 
     if (result is Result.Success)
         println(result.value)
-
+    else
+        if (result is Result.Failure)
+            println(result.message)
     exitProcess(0)
 }
