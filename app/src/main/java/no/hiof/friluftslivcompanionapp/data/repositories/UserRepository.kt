53ffc549128138bf5eba40d36addc886bd0e25d4
a5h71 <ashtim@hiof.no>
2023-10-01 14:14:17 +0200

    Added function to create new user document the UserRepository

--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/UserRepository.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/UserRepository.kt
@@ -1,8 +1,35 @@
 package no.hiof.friluftslivcompanionapp.data.repositories
 
-class UserRepository {
-    // Responsible for retrieving data from the db about a specific user.
-    // NOTE: also uses ActivityRepository, LifelistRepository, and PreferencesRepository
-    // to retrieve a single user's activity, lifelist, and user preferences.
-    // The instantiation of said user object would be best suited to the domain layer.
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import javax.inject.Inject
+// Responsible for retrieving data from the db about a specific user.
+// NOTE: also uses ActivityRepository, LifelistRepository, and PreferencesRepository
+// to retrieve a single user's activity, lifelist, and user preferences.
+// The instantiation of said user object would be best suited to the domain layer.
+class UserRepository @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth
+){
+    fun createUser(user: no.hiof.friluftslivcompanionapp.models.User, onComplete: (Boolean) -> Unit) {
+        val userId = auth.currentUser?.uid
+
+        if (userId != null) {
+            // Set the user document in Firestore with the Firebase Auth UID as the document ID
+            firestore.collection("users")
+                .document(userId)
+                .set(user, SetOptions.merge())
+                .addOnSuccessListener {
+                    onComplete(true)
+                }
+                .addOnFailureListener {
+                    onComplete(false)
+                }
+        } else {
+            onComplete(false)
+        }
+    }
+
+
 }
