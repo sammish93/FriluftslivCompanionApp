69a9be0f67bf44d6f89dfc73f99f892926de9cf0
a5h71 <ashtim@hiof.no>
2023-10-01 14:23:55 +0200

    Added new code to check if the create function in userRepository works as intended

--- app/src/main/java/no/hiof/friluftslivcompanionapp/SignInActivity.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/SignInActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -12,13 +11,21 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import com.firebase.ui.common.R
+import com.google.firebase.firestore.FirebaseFirestore
+import no.hiof.friluftslivcompanionapp.data.repositories.UserRepository
+import no.hiof.friluftslivcompanionapp.models.UserPreferences
 
 @AndroidEntryPoint
 class SignInActivity : AppCompatActivity() {
 
     @Inject
     lateinit var auth: FirebaseAuth
+
+    @Inject
+    lateinit var firestore : FirebaseFirestore
+
+    @Inject
+    lateinit var userRepository: UserRepository
 
     private val signInLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
@@ -49,6 +56,42 @@ class SignInActivity : AppCompatActivity() {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
             val user = FirebaseAuth.getInstance().currentUser
+
+            if (user != null) {
+                // Create or update the user document using the UserRepository
+                val userData = no.hiof.friluftslivcompanionapp.models.User(
+                    userId = user.uid,
+                    email = user.email,
+                    username = "default_username",
+                    preferences = UserPreferences(),
+                    lifelist = null,
+                    tripActivity = null
+                )
+
+                userRepository.createUser(userData) { success ->
+                    if (success) {
+                        val mainIntent = Intent(this, MainActivity::class.java)
+                        startActivity(mainIntent)
+                        finish()
+                    } else {
+                        Log.e("SignInActivity", "Error creating/updating user document")
+                    }
+                }
+            } else {
+                Log.e("SignInActivity", "User is null")
+            }
+        } else {
+            val errorMessage = "Sign-in failed. Please try again."
+            Log.e("SignInActivity", errorMessage)
+        }
+    }
+
+    /*
+    //This code is blocked out for now the above code is making sure the userRepository is working
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
             // User signed in successfully, you can handle this if needed
         } else {
             /*TODO add error message through pop-up or something*/
@@ -56,4 +99,7 @@ class SignInActivity : AppCompatActivity() {
             Log.e("SignInActivity", errorMessage)
         }
     }
+
+
+     */
 }
--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/db/FirebaseModule.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/db/FirebaseModule.kt
@@ -9,12 +9,24 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import no.hiof.friluftslivcompanionapp.data.repositories.UserRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
     @Provides
     fun auth(): FirebaseAuth = Firebase.auth
+
     @Provides
     fun firestore(): FirebaseFirestore = Firebase.firestore
+
+    //It is possible this will be deleted later. Adding this right now for
+    //the purpose of seeing if the user repo adds new doc
+    @Provides
+    fun provideUserRepository(
+        firestore: FirebaseFirestore,
+        auth: FirebaseAuth
+    ): UserRepository {
+        return UserRepository(firestore, auth)
+    }
 }
