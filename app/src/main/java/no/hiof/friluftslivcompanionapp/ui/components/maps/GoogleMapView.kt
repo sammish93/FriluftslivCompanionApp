4e5646c8d5bb9b26605584abb9075754b7784f5c
stian96 <stiankru@hiof.no>
2023-10-03 17:48:06 +0200

    implemented a GoogleMapView composable

---
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/maps/GoogleMapView.kt
@@ -0,0 +1,30 @@
+package no.hiof.friluftslivcompanionapp.ui.components.maps
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun GoogleMapView() {
+
+    val oslo = LatLng(59.9139, 10.7522)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(oslo, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = oslo),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+}
