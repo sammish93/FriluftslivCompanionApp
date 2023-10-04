bd7bcd22f7776be32525eb53720ee8a9ee7f8dca
Sammish <samd93@hotmail.co.uk>
2023-10-04 10:17:06 +0200

    added compose navigation dependency to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,26 +88,26 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     ksp("com.google.dagger:hilt-compiler:2.47")
 
+    // Navigation Compose - required to inject viewModels into NavHost routes
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
 
-    //Google Play need to have this for google authentication
+    // Google Play - need to have this for google authentication
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
-    //Firebase UI Library
+    // Firebase UI Library
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
 
     // Google Fonts
     implementation("androidx.compose.ui:ui-text-google-fonts:1.4.3")
 
-    //Google Play need to have this for google authentication
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-
-    //Firebase UI Library
+    // Firebase UI Library
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
 
