8030d51e04032f72b817939932a0d197028f12e1
Sammish <samd93@hotmail.co.uk>
2023-10-04 09:18:24 +0200

    added env variable path to weather API key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
     buildTypes {
         debug {
             buildConfigField("String", "EBIRD_API_KEY", "\"${System.getenv("EBIRD_API_KEY")}\"")
+            buildConfigField("String", "WEATHER_API_KEY", "\"${System.getenv("WEATHER_API_KEY")}\"")
         }
         release {
             isMinifyEnabled = false
@@ -34,6 +35,7 @@ android {
                 "proguard-rules.pro"
             )
             buildConfigField("String", "EBIRD_API_KEY", "\"${System.getenv("EBIRD_API_KEY")}\"")
+            buildConfigField("String", "WEATHER_API_KEY", "\"${System.getenv("WEATHER_API_KEY")}\"")
         }
     }
     compileOptions {
@@ -68,9 +70,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    // Possibly unnecessary - was used for BottomNavigation but they renamed it.
-    // implementation("androidx.compose.material:material:1.5.1")
-    //implementation("androidx.compose.material:material:1.5.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -102,10 +101,8 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
 
-
     // Google Fonts
     implementation("androidx.compose.ui:ui-text-google-fonts:1.4.3")
-
 
     //Google Play need to have this for google authentication
     implementation("com.google.android.gms:play-services-auth:20.7.0")
@@ -113,8 +110,7 @@ dependencies {
     //Firebase UI Library
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     implementation("com.firebaseui:firebase-ui-database:8.0.2")
+
     // Mockito
     testImplementation("org.mockito:mockito-core:5.5.0")
-
-
 }
