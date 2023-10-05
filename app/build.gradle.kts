c61975fb71c0ddce989c894831c31d096b5ff960
stian96 <stiankru@hiof.no>
2023-10-05 13:56:46 +0200

    removed copy of firebase ui library.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,9 +114,6 @@ dependencies {
     //Google Play need to have this for google authentication
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
-    //Firebase UI Library
-    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
-    implementation("com.firebaseui:firebase-ui-database:8.0.2")
     // Mockito
     testImplementation("org.mockito:mockito-core:5.5.0")
 
