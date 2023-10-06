b00533db995ce7014f341896d290dd9b330d0133
a5h71 <ashtim@hiof.no>
2023-10-06 09:42:15 +0200

    Removed comments from the values

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,13 @@ plugins {
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 
 }
-// Create a variable called keystorePropertiesFile and initialize it to your
-// keystore.properties file in the rootProject folder.
+
 val keystorePropertiesFile = rootProject.file("keystore.properties")
 
-// Initialize a new Properties() object called keystoreProperties.
+
 val keystoreProperties = Properties()
 
-// Load your keystore.properties file into the keystoreProperties object.
+
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 
