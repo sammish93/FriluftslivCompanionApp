0a0ba8a2dc79c306163b29e7288d0083587f4b41
Sammish <samd93@hotmail.co.uk>
2023-10-13 10:58:58 +0200

    updated DateFormatter to include a static function to return pretty date strings

--- app/src/main/java/no/hiof/friluftslivcompanionapp/domain/DateFormatter.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/domain/DateFormatter.kt
@@ -1,20 +1,68 @@
 package no.hiof.friluftslivcompanionapp.domain
 
+import androidx.compose.ui.text.capitalize
+import no.hiof.friluftslivcompanionapp.models.enums.SupportedLanguage
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 /**
  * A static object used to contain functions for formatting dates.
  */
 object DateFormatter {
 
-    // Takes a String in the unix timestamp format and returns a LocalDate object
+    /**
+     * @param unixTimestamp Takes a a unix timestamp as a parameter (e.g. "1697187432").
+     * @return Returns a LocalDate object.
+     */
     fun formatFromUnixTimestamp(unixTimestamp: String): LocalDate {
         val unixTimestamp = unixTimestamp.toLong()
         val instant = Instant.ofEpochSecond(unixTimestamp)
         val zoneId = ZoneId.systemDefault()
 
         return instant.atZone(zoneId).toLocalDate()
+    }
+
+    /**
+     * A static function designed to take a LocalDate object and return a pretty String.
+     * @param localDate Takes a LocalDate object.
+     * @param locale Takes a SupportedLanguage enum value. The default value is
+     * SupportedLanguage.ENGLISH.
+     * @return Returns a String in the format "2021-09-13" -> "Tuesday 13th September".
+     * In Norwegian this would be -> "Tirsdag 13 September" without the suffix.
+     */
+    fun formatToPrettyStringWithoutYear(localDate: LocalDate, locale: SupportedLanguage = SupportedLanguage.ENGLISH): String {
+        var localeObj: Locale? = null
+
+        when (locale) {
+            SupportedLanguage.ENGLISH -> localeObj = Locale.ENGLISH
+            SupportedLanguage.NORWEGIAN -> localeObj = Locale("no")
+            else -> return ""
+        }
+
+        val dayOfWeekFormatter = DateTimeFormatter.ofPattern("EEEE", localeObj)
+        val dayOfMonth = localDate.dayOfMonth
+        val monthFormatter = DateTimeFormatter.ofPattern("MMMM", localeObj)
+
+        val dayOfWeek = localDate.format(dayOfWeekFormatter).capitalize()
+        val month = localDate.format(monthFormatter).capitalize()
+
+        if (localeObj == Locale.ENGLISH) {
+            val dayOfMonthWithSuffix = when {
+                dayOfMonth in 11..13 -> "${dayOfMonth}th"
+                dayOfMonth % 10 == 1 -> "${dayOfMonth}st"
+                dayOfMonth % 10 == 2 -> "${dayOfMonth}nd"
+                dayOfMonth % 10 == 3 -> "${dayOfMonth}rd"
+                else -> "${dayOfMonth}th"
+            }
+
+            return "$dayOfWeek $dayOfMonthWithSuffix $month"
+        } else if (localeObj == Locale("no")) {
+            return "$dayOfWeek $dayOfMonth $month"
+        }
+
+        return ""
     }
 }
