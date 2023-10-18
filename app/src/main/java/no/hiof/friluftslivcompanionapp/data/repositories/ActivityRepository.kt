c8985f0136d181d06b3acd8db104e464700b2676
a5h71 <ashtim@hiof.no>
2023-10-18 10:50:20 +0200

    Added method to save an activity to db

--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/ActivityRepository.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/ActivityRepository.kt
@@ -1,29 +1,57 @@
 package no.hiof.friluftslivcompanionapp.data.repositories
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import no.hiof.friluftslivcompanionapp.models.Hike
+import no.hiof.friluftslivcompanionapp.models.TripActivity
+import no.hiof.friluftslivcompanionapp.models.User
 import java.time.LocalDate
+import java.util.Date
 import javax.inject.Inject
 
 class ActivityRepository @Inject constructor(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth
 ) {
 
-    suspend fun saveTripActivity(tripActivity: Map<LocalDate, Hike>, tripId: String) {
-        val tripActivityCollection = firestore.collection("trips").document(tripId).collection("activities")
 
-
-        val tripActivityMap = mapOf(
-            "activity" to tripActivity
-        )
-
+    suspend fun addHikeActivityToUser(date: Date, hike: Hike): Result<Boolean> = withContext(
+        Dispatchers.IO) {
         try {
+            val userId = auth.currentUser?.uid
+                ?: return@withContext Result.failure(Exception("No user logged in"))
 
-            val documentReference = tripActivityCollection.add(tripActivityMap).await()
-            println("Trip Activity saved with ID: ${documentReference.id}")
+            val userDocumentRef = firestore.collection("users").document(userId)
+
+            val hikeData = hikeToMap(hike)
+
+            val tripActivityUpdate = mapOf(
+                "activity.$date" to hikeData
+            )
+
+            userDocumentRef.update(tripActivityUpdate).await()
+
+            Result.success(true)
         } catch (e: Exception) {
-            println("Error saving Trip Activity: $e")
+
+            Result.failure(e)
         }
     }
+
+    private fun hikeToMap(hike: Hike): Map<String, Any?> {
+        return mapOf(
+            "documentId" to hike.documentId,
+            "startLocation" to hike.startLocation,
+            "endLocation" to hike.endLocation,
+            "description" to hike.description,
+            "duration" to hike.duration,
+            "distanceKm" to hike.distanceKm,
+            "difficulty" to hike.difficulty
+        )
+    }
+
 }
