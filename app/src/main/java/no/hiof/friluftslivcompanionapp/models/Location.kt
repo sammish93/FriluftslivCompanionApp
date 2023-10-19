1a91e210f33e59b1913df83769a8aedad1fb9c71
a5h71 <ashtim@hiof.no>
2023-10-19 15:03:08 +0200

    added toMap to Location in case we need it

--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/Location.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/Location.kt
@@ -1,4 +1,13 @@
 package no.hiof.friluftslivcompanionapp.models
 
 // The properties 'lat' and 'lon' (latitude and longitude) are currently mutable (var).
-data class Location(var lat: Double, var lon: Double)
+data class Location(var lat: Double, var lon: Double){
+    fun toMap(): Map<String, Double> {
+        return mapOf(
+            "lat" to lat,
+            "lon" to lon
+        )
+    }
+
+
+}
