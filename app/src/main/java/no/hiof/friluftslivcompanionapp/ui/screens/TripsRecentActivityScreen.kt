8e3f0912d5d6ace0033554109f1b9cafe2c6cd55
stian96 <stiankru@hiof.no>
2023-10-20 15:45:31 +0200

    implemented various composable´s for recent activity.

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsRecentActivityScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsRecentActivityScreen.kt
@@ -1,25 +1,23 @@
 package no.hiof.friluftslivcompanionapp.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
 import no.hiof.friluftslivcompanionapp.models.DummyTrip
-import no.hiof.friluftslivcompanionapp.models.enums.Screen
-import no.hiof.friluftslivcompanionapp.ui.components.CustomTabsBar
-import no.hiof.friluftslivcompanionapp.ui.theme.CustomTypography
 import no.hiof.friluftslivcompanionapp.viewmodels.TripsViewModel
 
 
@@ -33,19 +31,48 @@ fun TripsRecentActivityScreen(
 
 
 
-    Column(
+}
+
+@Composable
+fun TripsDetailScreen(trip: DummyTrip) {
+    Column {
+        Text(text = trip.city)
+        Text(text = trip.county)
+        Text(text = trip.type)
+        Text(text = trip.description)
+        Text(text = trip.duration)
+        Text(text = "${trip.difficulty} / 5 Difficulty")
+
+        // Må implementere en map veiw.
+    }
+}
+
+@Composable
+fun MapView(nodes: List<LatLng>) {
+
+    GoogleMap() {
+
+    }
+}
+
+@Composable
+fun TripCard(trip: DummyTrip, onClick: (DummyTrip) -> Unit) {
+
+    Card(
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable(onClick = { onClick(trip) })
     ) {
-        Text(
-            text = "This is the Recent Activity tab inside the Trips screen!",
-            style = CustomTypography.titleLarge,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Center,
-            modifier = modifier.fillMaxSize()
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ){
+
+        }
+
     }
 }
 
