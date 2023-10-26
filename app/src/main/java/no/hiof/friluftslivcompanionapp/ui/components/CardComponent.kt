cb33137b52a72f308024c88f81b99c5e3e08e2ee
a5h71 <ashtim@hiof.no>
2023-10-26 10:09:13 +0200

    Made some changes to CardComponent to make the first card to appear bigger in the carousel

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/CardComponent.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/CardComponent.kt
@@ -3,6 +3,10 @@ package no.hiof.friluftslivcompanionapp.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
@@ -16,13 +20,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import no.hiof.friluftslivcompanionapp.R
 import no.hiof.friluftslivcompanionapp.ui.components.items.CardItem
 
