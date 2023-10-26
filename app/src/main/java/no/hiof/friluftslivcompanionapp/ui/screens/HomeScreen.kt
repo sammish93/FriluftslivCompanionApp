09a1a53d5fc637e12d233900dc6d6f31f2637846
a5h71 <ashtim@hiof.no>
2023-10-26 15:21:46 +0200

    Removed unnecessary stuff from HomeScreen

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/HomeScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
