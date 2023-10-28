234fb37b3a38dc8e2b7968d376072e834b0c748b
a5h71 <ashtim@hiof.no>
2023-10-28 14:47:29 +0200

    Changed structure of TripActivity

--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/TripActivity.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/TripActivity.kt
@@ -3,4 +3,17 @@ package no.hiof.friluftslivcompanionapp.models
 import java.util.Date
 
 // Datetime vs Date - Datetime seems to cause compile errors with kotlin. Date supports time alse.
-data class TripActivity(val tripActivity: Map<Date, Trip>)
+data class TripActivity(
+    val trip: Trip,
+    val date: Date
+) {
+    fun toMap(): Map<String, Any> {
+
+        val result = mutableMapOf<String, Any>()
+
+        result["trip"] = this.trip.toMap()
+        result["date"] = this.date
+
+        return result
+    }
+}
