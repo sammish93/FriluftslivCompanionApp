efe9372f7835b1165910961b8bb43d5d719b6ede
stian96 <stiankru@hiof.no>
2023-10-28 23:06:47 +0200

    created a TripItem composable.

---
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/items/TripItem.kt
@@ -0,0 +1,9 @@
+package no.hiof.friluftslivcompanionapp.ui.components.items
+
+import androidx.compose.runtime.Composable
+import no.hiof.friluftslivcompanionapp.models.Trip
+
+@Composable
+fun TripItem(trip: Trip) {
+    // Show information about the trips here.
+}
