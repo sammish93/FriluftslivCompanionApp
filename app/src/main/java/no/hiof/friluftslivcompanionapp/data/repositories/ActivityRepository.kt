f8a511c81b61a623a0f0d018e50850b5f6042bf1
a5h71 <ashtim@hiof.no>
2023-11-01 13:11:52 +0100

    cleaned up the code, removed commented out stuff

--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/ActivityRepository.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/repositories/ActivityRepository.kt
@@ -9,11 +9,8 @@ import kotlinx.coroutines.withContext
 import no.hiof.friluftslivcompanionapp.models.Hike
 import no.hiof.friluftslivcompanionapp.models.Trip
 import no.hiof.friluftslivcompanionapp.models.TripActivity
-import java.text.ParseException
-import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
-import java.util.Locale
 import javax.inject.Inject
 
 class ActivityRepository @Inject constructor(
@@ -143,6 +140,8 @@ class ActivityRepository @Inject constructor(
         }
     }
 
+
+
     suspend fun getTotalKilometersForYear(): Double {
         val allTrips = getUserTripActivities()
         if (allTrips is OperationResult.Success) {
@@ -163,111 +162,6 @@ class ActivityRepository @Inject constructor(
         calendar.set(Calendar.MILLISECOND, 0)
         return calendar.time
     }
-
-
-
-
-    /*
-
-    suspend fun getAllUserActivities(): OperationResult<List<TripActivity>> {
-        return withContext(Dispatchers.IO) {
-            val logTag = "UserActivitiesLog" // Define a log tag
-
-            try {
-                Log.d(logTag, "Starting retrieval of user activities")
-
-                val userId = auth.currentUser?.uid
-                if (userId == null) {
-                    Log.e(logTag, "No user logged in")
-                    return@withContext OperationResult.Error(Exception("No user logged in"))
-                }
-
-                Log.d(logTag, "Logged-in user ID: $userId")
-
-                val activitySubcollectionRef = firestore.collection("users").document(userId).collection("tripActivity")
-                Log.d(logTag, "Retrieving activities from Firestore for user: $userId")
-
-                val querySnapshot = activitySubcollectionRef.get().await()
-
-                val tripActivities: MutableList<TripActivity> = mutableListOf()
-
-                for (document in querySnapshot.documents) {
-                    Log.d(logTag, "Processing document: ${document.id}")
-
-                    val dateKey = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(document.id)
-                    val tripValue = document.toObject(Hike::class.java)
-
-                    if (dateKey != null && tripValue != null) {
-                        val tripActivityMap: MutableMap<Date, Trip> = mutableMapOf()
-                        tripActivityMap[dateKey] = tripValue
-                        tripActivities.add(TripActivity(tripActivityMap))
-                    }
-                }
-
-                Log.d(logTag, "Successfully compiled list of TripActivities. Total count: ${tripActivities.size}")
-
-                OperationResult.Success(tripActivities)
-
-            } catch (e: Exception) {
-                OperationResult.Error(e)
-            }
-        }
-    }
-
-
-
-
-    suspend fun getUserActivityByDate(date: Date): Result<Trip?> = withContext(Dispatchers.IO) {
-        try {
-            val userId = auth.currentUser?.uid
-                ?: return@withContext Result.failure(Exception("No user logged in"))
-
-            val userDocumentRef = firestore.collection("users").document(userId)
-            val userDocument = userDocumentRef.get().await()
-
-            if (!userDocument.exists()) {
-                return@withContext Result.failure(Exception("User not found"))
-            }
-
-            val tripActivityData = userDocument.toObject(TripActivity::class.java)?.tripActivity
-                ?: return@withContext Result.failure(Exception("No trip activity found for user"))
-
-            val tripForGivenDate = tripActivityData[date]
-
-            if (tripForGivenDate != null) {
-                return@withContext Result.success(tripForGivenDate)
-            } else {
-                return@withContext Result.failure(Exception("No trip activity found for the given date"))
-            }
-        } catch (e: Exception) {
-            return@withContext Result.failure(e)
-        }
-    }
-
-     */
-
-    suspend fun getTotalDistanceCompleted(): Result<Double> = withContext(Dispatchers.IO) {
-        try {
-            val userId = auth.currentUser?.uid
-                ?: return@withContext Result.failure(Exception("No user logged in"))
-
-            val userActivityCollectionRef = firestore.collection("users").document(userId).collection("tripActivity")
-            val userActivities = userActivityCollectionRef.get().await()
-
-            var totalDistance = 0.0
-
-            for (document in userActivities) {
-                val trip = document.toObject(Trip::class.java)
-                totalDistance += trip.distanceKm ?: 0.0
-            }
-
-            return@withContext Result.success(totalDistance)
-        } catch (e: Exception) {
-            return@withContext Result.failure(e)
-        }
-    }
-
-
 
 
 }
