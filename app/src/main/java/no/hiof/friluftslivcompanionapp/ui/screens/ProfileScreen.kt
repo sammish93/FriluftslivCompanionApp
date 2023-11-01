3b275df2fe21a2db2a530ef6f51104ff16a11436
a5h71 <ashtim@hiof.no>
2023-11-01 10:57:30 +0100

    Removed the extra variable and launchedEffect

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/ProfileScreen.kt
@@ -99,11 +99,7 @@ fun ProfileScreen(
         userViewModel.fetchTotalKilometersForTheYear()
         userViewModel.fetchSpeciesCountForThisYear()
     }
-    val tripCount by userViewModel.tripCountForTheYear.collectAsState()
 
-    LaunchedEffect(true) {
-        userViewModel.fetchTripCountForTheYear()
-    }
 
 
     Scaffold(
