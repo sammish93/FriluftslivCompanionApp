596eea3e97c0e22b7da35bd8a1f9fd34b77b39a9
a5h71 <ashtim@hiof.no>
2023-11-01 15:38:09 +0100

    Added tomap and frommap to UserPreferences

--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/UserPreferences.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/UserPreferences.kt
@@ -7,4 +7,21 @@ data class UserPreferences(
     var displayPicture: DisplayPicture = DisplayPicture.DP_DEFAULT,
     var darkModeEnabled: Boolean = false,
     var language: SupportedLanguage = SupportedLanguage.ENGLISH
-)
+){
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "displayPicture" to displayPicture.name,
+            "darkModeEnabled" to darkModeEnabled,
+            "language" to language.name )
+    }
+
+    companion object {
+        fun fromMap(map: Map<String, Any?>): UserPreferences {
+            return UserPreferences(
+                displayPicture = DisplayPicture.valueOf(map["displayPicture"] as String),
+                darkModeEnabled = map["darkModeEnabled"] as Boolean,
+                language = SupportedLanguage.valueOf(map["language"] as String)
+            )
+        }
+    }
+}
