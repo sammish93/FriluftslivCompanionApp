c94281ba8fdfbdd8cf927a691076033555beda46
stian96 <stiankru@hiof.no>
2023-11-02 12:45:41 +0100

    removed unused code from TripsScreen.kt

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
@@ -1,23 +1,12 @@
 package no.hiof.friluftslivcompanionapp.ui.screens
 
 import android.location.Geocoder
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,16 +14,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.firebase.firestore.GeoPoint
 import no.hiof.friluftslivcompanionapp.R
 import no.hiof.friluftslivcompanionapp.models.enums.DefaultLocation
-import no.hiof.friluftslivcompanionapp.ui.components.maps.GoogleMapCreate
 import no.hiof.friluftslivcompanionapp.ui.components.maps.GoogleMapTripStartNodes
 import no.hiof.friluftslivcompanionapp.viewmodels.TripsViewModel
 import no.hiof.friluftslivcompanionapp.viewmodels.UserViewModel
@@ -93,40 +79,5 @@ fun TripsScreen(
             modifier.padding(contentPadding)
         )
     }
-
-    /*
-    Box(
-        modifier = Modifier
-            .padding(top = 24.dp, start = 8.dp, bottom = 24.dp, end = 8.dp)
-            .fillMaxSize()
-    ) {
-        Column {
-            Card(
-                shape = RoundedCornerShape(12.dp),
-                elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
-                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer),
-            ) {
-                Column(
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    Text(text = "Trips near:")
-                    Text(text = "$addressLine")
-                }
-            }
-            Spacer(modifier = Modifier.height(24.dp))
-            Column(
-                modifier = Modifier.clip(RoundedCornerShape(20.dp)),
-                verticalArrangement = Arrangement.Bottom
-            ) {
-                GoogleMapTripStartNodes(
-                    navController = navController,
-                    tripsViewModel = viewModel,
-                    userViewModel = userViewModel,
-                    trips = tripsInArea
-                )
-            }
-        }
-    }
- */
 }
 
