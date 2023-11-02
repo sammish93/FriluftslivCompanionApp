8acf4e866a002c9ad4a8618ae45245c0ace923fb
stian96 <stiankru@hiof.no>
2023-11-02 16:11:31 +0100

    fixed "Search this area" button to update map with trips based on cameras current position.

--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/maps/GoogleMapTripStartNode.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/components/maps/GoogleMapTripStartNode.kt
@@ -13,8 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,10 +35,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import no.hiof.friluftslivcompanionapp.R
 import no.hiof.friluftslivcompanionapp.domain.TripFactory
 import no.hiof.friluftslivcompanionapp.models.Hike
-import no.hiof.friluftslivcompanionapp.models.enums.DefaultLocation
 import no.hiof.friluftslivcompanionapp.models.enums.Screen
-import no.hiof.friluftslivcompanionapp.utils.getCameraPosition
-import no.hiof.friluftslivcompanionapp.utils.getLastKnownLocation
 import no.hiof.friluftslivcompanionapp.viewmodels.TripsViewModel
 import no.hiof.friluftslivcompanionapp.viewmodels.UserViewModel
 
@@ -53,35 +48,15 @@ fun GoogleMapTripStartNodes(
     modifier: Modifier = Modifier,
     onSearchAreaRequested: (LatLng) -> Unit) {
 
-    val mapLoadedState = remember { mutableStateOf(false) }
-    val bounds = computeBounds(trips)
-    val latestBoundsState = rememberUpdatedState(bounds)
-
+    val cameraPositionState = rememberCameraPositionState()
+    val latestBoundsState = rememberUpdatedState(computeBounds(trips))
     val userState by userViewModel.state.collectAsState()
-    val lastKnownLocation = userState.lastKnownLocation
-    val userLocation = getLastKnownLocation(lastKnownLocation)
 
     val mapProperties =
         MapProperties(
             isMyLocationEnabled = userState.isLocationPermissionGranted,
             mapType = MapType.TERRAIN
         )
-
-    val cameraPosition = getCameraPosition(
-        userLocation ?: LatLng(
-            DefaultLocation.OSLO.lat,
-            DefaultLocation.OSLO.lon
-        ), if (userState.isLocationPermissionGranted) 12f else 5f
-    )
-
-    /*
-    val cameraPositionState = rememberCameraPositionState {
-        position = (if (latestBoundsState.value != null) getCameraPosition(
-            latestBoundsState.value!!.center,
-            12f
-        ) else null) ?: cameraPosition }
-     */
-    val cameraPositionState = rememberCameraPositionState()
 
     fun searchInCurrentMapArea() {
         val currentCameraPosition = cameraPositionState.position.target
@@ -92,7 +67,6 @@ fun GoogleMapTripStartNodes(
         GoogleMap(
             cameraPositionState = cameraPositionState,
             properties = mapProperties,
-            onMapLoaded = { mapLoadedState.value = true },
             modifier = Modifier.fillMaxWidth(),
             uiSettings = MapUiSettings(zoomControlsEnabled = true, zoomGesturesEnabled = true)
         ) {
@@ -118,15 +92,10 @@ fun GoogleMapTripStartNodes(
         )
     }
 
-    val initialLoad = remember { mutableStateOf(true) }
-    LaunchedEffect(initialLoad.value) {
-        if (initialLoad.value) {
-            latestBoundsState.value?.let { bounds ->
-                val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100)
-                cameraPositionState.move(cameraUpdate)
-
-                initialLoad.value = false
-            }
+    LaunchedEffect(Unit) {
+        latestBoundsState.value?.let { bounds ->
+            val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100)
+            cameraPositionState.move(cameraUpdate)
         }
     }
 }
--- app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/ui/screens/TripsScreen.kt
@@ -3,30 +3,21 @@ package no.hiof.friluftslivcompanionapp.ui.screens
 import android.location.Address
 import android.location.Geocoder
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.firebase.firestore.GeoPoint
-import no.hiof.friluftslivcompanionapp.R
 import no.hiof.friluftslivcompanionapp.data.states.UserState
 import no.hiof.friluftslivcompanionapp.models.enums.DefaultLocation
 import no.hiof.friluftslivcompanionapp.ui.components.maps.GoogleMapTripStartNodes
 import no.hiof.friluftslivcompanionapp.viewmodels.TripsViewModel
 import no.hiof.friluftslivcompanionapp.viewmodels.UserViewModel
-import java.util.Locale
 
 @Composable
 fun TripsScreen(
@@ -35,12 +26,9 @@ fun TripsScreen(
     userViewModel: UserViewModel
 ) {
     val userState by userViewModel.state.collectAsState()
-
-    val geocoder = Geocoder(LocalContext.current, Locale.getDefault())
-    val location = getLocation(userState, geocoder)
-
     val tripsInArea by viewModel.hikes.collectAsState()
-    LaunchedEffect(userState) {
+
+    LaunchedEffect(Unit) {
         val geoPoint = userState.lastKnownLocation?.let { GeoPoint(it.latitude, it.longitude) }
         if (geoPoint != null) {
             viewModel.getTripsNearUsersLocation(geoPoint, 50.0, 5)
