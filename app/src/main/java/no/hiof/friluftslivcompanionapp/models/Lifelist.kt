9854582175bac1add64a8b7a5c15a9732988a601
a5h71 <ashtim@hiof.no>
2023-11-12 11:43:54 +0100

    Added toMap to Lifelist

--- app/src/main/java/no/hiof/friluftslivcompanionapp/models/Lifelist.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/models/Lifelist.kt
@@ -1,3 +1,11 @@
 package no.hiof.friluftslivcompanionapp.models
 
-data class Lifelist(val sightings: FloraFaunaSighting)
+data class Lifelist(
+    val sightings: FloraFaunaSighting
+){
+    fun toMap(): Map<String, Any?>{
+        return mapOf(
+            "sightings" to sightings.toMap()
+        )
+    }
+}
