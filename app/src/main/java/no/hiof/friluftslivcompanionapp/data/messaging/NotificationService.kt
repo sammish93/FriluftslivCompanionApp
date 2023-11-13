ab062283d514835a3f7e532072134bfbb730c85e
stian96 <stiankru@hiof.no>
2023-11-13 19:20:30 +0100

    refactored NotificationService to use notificationHelper

--- app/src/main/java/no/hiof/friluftslivcompanionapp/data/messaging/NotificationService.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/messaging/NotificationService.kt
@@ -7,15 +7,9 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import no.hiof.friluftslivcompanionapp.R
 
 // Reference: https://developer.android.com/develop/ui/views/notifications/build-notification
 class NotificationService : FirebaseMessagingService() {
-
-    companion object {
-        const val CHANNEL_ID = "friluftslivcompanion_notification_channel"
-        const val NOTIFICATION_ID = 1
-    }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -29,31 +23,8 @@ class NotificationService : FirebaseMessagingService() {
 
         remoteMessage.notification?.let {
             Log.d("FCM", "Notification Message Body: ${it.body}")
-            showNotification(it)
+            NotificationHelper.showMessage(this, it.title!!, it.body!!)
         }
     }
 
-    private fun showNotification(notification: RemoteMessage.Notification) {
-        val notificationBuilder = setNotificationContent(
-            R.mipmap.ic_launcher_round,
-            notification.title!!,
-            notification.body!!
-        )
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val channel = NotificationChannel(CHANNEL_ID,
-            "Notification service",
-            NotificationManager.IMPORTANCE_HIGH
-        )
-        notificationManager.createNotificationChannel(channel)
-        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
-    }
-
-    private fun setNotificationContent(icon: Int, title: String, body: String): NotificationCompat.Builder {
-        return NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(icon)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setPriority(NotificationCompat.PRIORITY_MAX)
-    }
 }
