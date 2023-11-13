d4a83c6eae3c5ba70992b77413820e7b08069241
stian96 <stiankru@hiof.no>
2023-11-13 19:19:43 +0100

    created a NotificationHelper object.

---
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/data/messaging/NotificationHelper.kt
@@ -0,0 +1,32 @@
+package no.hiof.friluftslivcompanionapp.data.messaging
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import no.hiof.friluftslivcompanionapp.R
+
+// Reference: https://developer.android.com/develop/ui/views/notifications/build-notification
+object NotificationHelper {
+
+    private const val CHANNEL_ID = "friluftslivcompanion_notification_channel"
+    private const val NOTIFICATION_ID = 1
+
+    fun showMessage(context: Context, title: String, body: String) {
+        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            "Notification Service",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        manager.createNotificationChannel(channel)
+        manager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+}
