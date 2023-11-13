b4147d4dd1e6998e13cdf41b574b09bc5e8b9d00
stian96 <stiankru@hiof.no>
2023-11-13 21:11:34 +0100

    set up the background job in the application class.

--- app/src/main/java/no/hiof/friluftslivcompanionapp/FriluftslivCompanionApp.kt
+++ app/src/main/java/no/hiof/friluftslivcompanionapp/FriluftslivCompanionApp.kt
@@ -1,8 +1,40 @@
 package no.hiof.friluftslivcompanionapp
 
 import android.app.Application
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
+import no.hiof.friluftslivcompanionapp.data.messaging.backgroundtask.CheckWeatherJob
+import java.util.concurrent.TimeUnit
 
 
 @HiltAndroidApp
-class FriluftslivCompanionApp : Application()
+class FriluftslivCompanionApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setUpWorkManager()
+    }
+
+    private fun setUpWorkManager() {
+        // Runs the job every 6 hour = 4 times a day.
+        val weatherCheckRequest = getWorkRequestBuilder()
+        enqueueJob(this, weatherCheckRequest)
+    }
+
+    private fun getWorkRequestBuilder(hourlyInterval: Long=6): PeriodicWorkRequest {
+        return PeriodicWorkRequestBuilder<CheckWeatherJob>(hourlyInterval, TimeUnit.HOURS)
+            .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+            .build()
+    }
+
+    private fun enqueueJob(context: Context, job: PeriodicWorkRequest) {
+        WorkManager.getInstance(context)
+            .enqueueUniquePeriodicWork("CheckWeatherWork", ExistingPeriodicWorkPolicy.KEEP, job)
+    }
+
+}
